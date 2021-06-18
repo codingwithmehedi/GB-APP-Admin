009d83bc2bc01ee304683e2548ea0c23a92d1f18
MD MEHEDI FAKIR <67342151+mdmehedifakir@users.noreply.github.com>
2021-06-18 09:59:39 +0600

    GB Admin Panel

---
+++ app/src/main/java/bd/codingwithmehedi/gbadmin/ui/home/HomeFragment.kt
@@ -0,0 +1,58 @@
+package bd.codingwithmehedi.gbadmin.ui.home
+
+import android.os.Bundle
+import android.view.*
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import bd.codingwithmehedi.gbadmin.R
+import bd.codingwithmehedi.gbadmin.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.add_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+        if (item.itemId.equals(R.id.item_add)){
+            Toast.makeText(context,"add",Toast.LENGTH_LONG).show()
+        }
+    }
+}
